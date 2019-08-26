34c6d1640d7bffc442d5b0f11cee683c97aaad14
DeniskaRediska <grj.den@gmail.com>
2019-08-26 13:30:02 +0300

    update(config description)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## v1.0.5
+* update config description
+* update lib
 ## v1.0.3
 * reformat
 ## v1.0.2
--- conf/remote_config.go
+++ conf/remote_config.go
@@ -1,5 +1,5 @@
 package conf
 
 type RemoteConfig struct {
-	BaseLogDirectory string `valid:"required~Required"`
+	BaseLogDirectory string `valid:"required~Required" schema:"Путь до хранилища логов,корневая директория, в которую будут сохраняться логи"`
 }
